93b6230428e41a925b29494d8b3b6fc0c678f7ed
unknown <khurram.raftaz@MS-LT-38.moftak.com>
2016-09-06 15:56:35 +0500

    modified

--- ActiveCompaign.Services/ContactService.cs
+++ ActiveCompaign.Services/ContactService.cs
@@ -1,27 +1,21 @@
-﻿// Decompiled with JetBrains decompiler
-// Type: ActiveCampaign.Services.ContactService
-// Assembly: ActiveCampaign, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null
-// MVID: 0B732A39-7F2E-43E4-B7C5-91A9F4429CA3
-// Assembly location: C:\Users\khurram.raftaz\Desktop\Temp\ActiveCampaign-master\ActiveCampaign-master\WebApplication1\bin\ActiveCampaign.dll
-
-using ActiveCampaign.Models;
+﻿using ActiveCampaign.Models;
 using System;
 using System.Collections.Generic;
 
 namespace ActiveCampaign.Services
 {
-  public class ContactService
-  {
-    private readonly ActiveCampaignConnectorService service;
-
-    public ContactService(ActiveCampaignConnectorService service)
+    public class ContactService
     {
-      this.service = service;
-    }
+        private readonly ActiveCampaignConnectorService service;
 
-    public bool AddContact(BasicContactInfo basicContactInfo, IEnumerable<BasicContactList> contactLists)
-    {
-      Dictionary<string, string> postParameters = new Dictionary<string, string>()
+        public ContactService(ActiveCampaignConnectorService service)
+        {
+            this.service = service;
+        }
+
+        public bool AddContact(BasicContactInfo basicContactInfo, IEnumerable<BasicContactList> contactLists)
+        {
+            Dictionary<string, string> postParameters = new Dictionary<string, string>()
       {
         {
           "email",
@@ -48,17 +42,17 @@ namespace ActiveCampaign.Services
           string.Join(",", (IEnumerable<string>) basicContactInfo.Tags)
         }
       };
-      foreach (BasicContactList contactList in contactLists)
-      {
-        postParameters.Add(string.Format("p[{0}]", (object) contactList.Id), contactList.Id.ToString());
-        postParameters.Add(string.Format("status[{0}]", (object) contactList.Id), ((int) contactList.Status).ToString());
-      }
-      return this.service.IsRequestSuccessfull((IReadOnlyDictionary<string, object>) this.service.SendRequest("contact_add", (Dictionary<string, string>) null, postParameters));
-    }
+            foreach (BasicContactList contactList in contactLists)
+            {
+                postParameters.Add(string.Format("p[{0}]", (object)contactList.Id), contactList.Id.ToString());
+                postParameters.Add(string.Format("status[{0}]", (object)contactList.Id), ((int)contactList.Status).ToString());
+            }
+            return this.service.IsRequestSuccessfull((IReadOnlyDictionary<string, object>)this.service.SendRequest("contact_add", (Dictionary<string, string>)null, postParameters));
+        }
 
-    public Dictionary<string, object> SyncContact(BasicContactInfo basicContactInfo, IEnumerable<BasicContactList> contactLists)
-    {
-      Dictionary<string, string> postParameters = new Dictionary<string, string>()
+        public Dictionary<string, object> SyncContact(BasicContactInfo basicContactInfo, IEnumerable<BasicContactList> contactLists)
+        {
+            Dictionary<string, string> postParameters = new Dictionary<string, string>()
       {
         {
           "email",
@@ -89,21 +83,91 @@ namespace ActiveCampaign.Services
           string.Join(",", (IEnumerable<string>) basicContactInfo.Tags)
         }
       };
-      foreach (BasicContactList contactList in contactLists)
+            foreach (BasicContactList contactList in contactLists)
+            {
+                postParameters.Add(string.Format("p[{0}]", (object)contactList.Id), contactList.Id.ToString());
+                postParameters.Add(string.Format("status[{0}]", (object)contactList.Id), contactList.Status.ToString("D"));
+                postParameters.Add(string.Format("noresponders[{0}]", (object)contactList.Id), Convert.ToInt32(contactList.Noresponders).ToString());
+                postParameters.Add(string.Format("sdate[{0}]", (object)contactList.Id), contactList.SubscribeDate);
+                postParameters.Add(string.Format("instantresponders[{0}]", (object)contactList.Id), Convert.ToInt32(contactList.InstantResponders).ToString());
+                postParameters.Add(string.Format("lastmessage[{0}]", (object)contactList.Id), Convert.ToInt32(contactList.LastMessage).ToString());
+            }
+            if (basicContactInfo.Fields != null)
+            {
+                foreach (Field field in basicContactInfo.Fields)
+                    postParameters.Add(string.Format("field[{0},0]", field.Id.HasValue ? (object)field.Id.ToString() : (object)field.Name), field.Value);
+            }
+            return this.service.SendRequest("contact_sync", (Dictionary<string, string>)null, postParameters);
+        }
+        public string AddNewContact(string firstName, string lastName, string email, string listTypeId)
+        {
+            Dictionary<string, string> postParameters = new Dictionary<string, string>()
       {
-        postParameters.Add(string.Format("p[{0}]", (object) contactList.Id), contactList.Id.ToString());
-        postParameters.Add(string.Format("status[{0}]", (object) contactList.Id), contactList.Status.ToString("D"));
-        postParameters.Add(string.Format("noresponders[{0}]", (object) contactList.Id), Convert.ToInt32(contactList.Noresponders).ToString());
-        postParameters.Add(string.Format("sdate[{0}]", (object) contactList.Id), contactList.SubscribeDate);
-        postParameters.Add(string.Format("instantresponders[{0}]", (object) contactList.Id), Convert.ToInt32(contactList.InstantResponders).ToString());
-        postParameters.Add(string.Format("lastmessage[{0}]", (object) contactList.Id), Convert.ToInt32(contactList.LastMessage).ToString());
-      }
-      if (basicContactInfo.Fields != null)
+        {
+          "email",
+          email
+        },
+        {
+          "first_name",
+          firstName
+        },
+        {
+          "last_name",
+          lastName
+        }
+        ,
+        {
+            "p["+listTypeId+"]",
+           listTypeId
+        },
+        {
+        "status["+listTypeId+"]",
+        "1"
+        }
+      };
+            var response = this.service.Request((IReadOnlyDictionary<string, object>)this.service.SendRequest("contact_add", (Dictionary<string, string>)null, postParameters));
+            return response;
+        }
+
+        public string UnSubscribe(string email, string listTypeId)
+        {
+            Dictionary<string, string> postParameters = new Dictionary<string, string>()
       {
-        foreach (Field field in basicContactInfo.Fields)
-          postParameters.Add(string.Format("field[{0},0]", field.Id.HasValue ? (object) field.Id.ToString() : (object) field.Name), field.Value);
-      }
-      return this.service.SendRequest("contact_sync", (Dictionary<string, string>) null, postParameters);
+        {
+          "email",
+          email
+        },
+        {
+            "p["+listTypeId+"]",
+           listTypeId
+        },
+        {
+        "status["+listTypeId+"]",
+        "2"
+        }
+
+      };
+            return this.service.Request((IReadOnlyDictionary<string, object>)this.service.SendRequest("contact_sync", (Dictionary<string, string>)null, postParameters));
+        }
+        public string Subscribe(string email, string listTypeId)
+        {
+            Dictionary<string, string> postParameters = new Dictionary<string, string>()
+      {
+        {
+          "email",
+          email
+        },
+        {
+            "p["+listTypeId+"]",
+           listTypeId
+        },
+        {
+        "status["+listTypeId+"]",
+        "1"
+        }
+
+      };
+            return this.service.Request((IReadOnlyDictionary<string, object>)this.service.SendRequest("contact_sync", (Dictionary<string, string>)null, postParameters));
+        }
     }
-  }
 }
